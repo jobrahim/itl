0dab8131edc97ee90c85b0acb44c9edc54e748fc
Jorge Ibrahim Bastidas Castellanos <jicastellanos@latam.stefanini.com>
2021-08-11 21:19:29 -0300

    eliminada carpeta auth

--- src/auth/auth.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import {JwtStrategy} from './JwtStrategy';
-
-@Module({
-    providers: [JwtStrategy],
-    exports: [JwtStrategy],
-  })
-export class AuthModule {}
--- src/container-types/container-types.module.ts
+++ src/container-types/container-types.module.ts
@@ -1,17 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ContainerTypesService } from './container-types.service';
 import { ContainerTypesController } from './container-types.controller';
-import { AuthModule } from 'src/auth/auth.module';
 import { ContainerTypeEntity } from './entities/container-type.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    AuthModule,
-    TypeOrmModule.forFeature([ContainerTypeEntity])
-  ],
-  controllers:[ContainerTypesController],
-  providers: [ContainerTypesService]
+  imports: [TypeOrmModule.forFeature([ContainerTypeEntity])],
+  controllers: [ContainerTypesController],
+  providers: [ContainerTypesService],
 })
 export class ContainerTypesModule {}
-
