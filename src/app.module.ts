fb717ca85cd0e5215021e58978af3bf8bcc9cc21
Jorge Ibrahim Bastidas Castellanos <jicastellanos@latam.stefanini.com>
2021-08-12 09:24:35 -0300

    funcionando con error typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ContainerTypesController } from './container-types/container-types.controller';
 import { ContainerTypesModule } from './container-types/container-types.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ContainerTypeEntity } from './container-types/entities/container-type.entity';
 
 @Module({
-  imports: [ContainerTypesModule],
-  controllers: [AppController, ContainerTypesController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `env/${process.env.NODE_ENV || 'local'}.env`,
+    }),
+    //config typeORM
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('SQL_HOST'),
+        port: +config.get('SQL_PORT'),
+        username: config.get('SQL_USER'),
+        password: config.get('SQL_PASSWORD'),
+        database: config.get('SQL_DATABASE'),
+        entities: [ContainerTypeEntity],
+        synchronize: true,
+        options: {
+          encrypt: false,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    //end tyoe orm
+    ContainerTypesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
